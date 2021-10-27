d5b6571f9a2d907615f16649705d9a538cd32386
dedanmsafari <dedan.msafari@gmail.com>
2021-10-27 16:33:23 +0300

    Restaurant card file

---
+++ src/features/restaurants/components/restaurant-info-card.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+import { SvgXml } from "react-native-svg";
+import { Card } from "react-native-paper";
+import { Image, View, Text } from "react-native";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+const Titletext = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const CardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const RestaurantInfoCard = ({ Restaurant = {} }) => {
+  const {
+    name = "Vegitel",
+    address = "57th Street Avenue",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    rating = 4,
+    isClosedTemporarily = true,
+    isOpenNow = true,
+    photos = ["https://picsum.photos/200/300"],
+  } = Restaurant;
+
+  const ratingArray = Array.from({ length: Math.floor(rating) });
+
+  console.log(ratingArray);
+  return (
+    <RestaurantCard>
+      <CardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Titletext>{name}</Titletext>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                Temporarily Closed
+              </Text>
+            )}
+            <View style={{ paddingLeft: 16 }} />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <View style={{ paddingLeft: 16 }} />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
