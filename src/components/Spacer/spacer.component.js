3a6dfff956041f4755c030bbd2eeffdbbd9439fa
dedanmsafari <dedan.msafari@gmail.com>
2021-10-28 12:32:11 +0300

    spacer Component

---
+++ src/components/Spacer/spacer.component.js
@@ -0,0 +1,35 @@
+import styled, { useTheme } from "styled-components";
+import React from "react";
+const positionVariant = {
+  top: "marginTop",
+  right: "marginRight",
+  bottom: "marginBottom",
+  left: "marginLeft",
+};
+
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+const getVariant = (position, size, theme) => {
+  const property = positionVariant[position];
+  const getSize = sizeVariant[size];
+  const value = theme.space[getSize];
+  return `${property}:${value}`;
+};
+
+const SpacerView = styled.View`
+  ${({ variant }) => variant}
+`;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
+};
