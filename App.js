2e08621c18098fc4038669ed2982dccce8e319bb
dedanmsafari <dedan.msafari@gmail.com>
2021-10-30 21:25:30 +0300

    minor app and package json change

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { View, Text } from "react-native";
 import { ThemeProvider } from "styled-components";
 import {
   useFonts as useOswald,
@@ -7,7 +8,62 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { Ionicons, Feather } from "@expo/vector-icons";
 import { theme } from "./src/infrastructure/theme";
+import { SafeAreaView } from "./src/utils/safeArea.util.component";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function MapsScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Maps</Text>
+    </SafeAreaView>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Settings!</Text>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Maps: "map",
+  Settings: "settings-outline",
+};
+// const tabBarIcon =
+//   (route) =>
+//   ({ color, size }) => {
+//     let iconName;
+
+//     if (route.name === "Restaurants") {
+//       iconName = "restaurant";
+//     } else if (route.name === "Maps") {
+//       iconName = "map";
+//     } else if (route.name === "Settings") {
+//       iconName = "settings-outline";
+//     }
+
+//     return <Ionicons name={iconName} size={size} color={color} />;
+//   };
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    // tabBarIcon: tabBarIcon(route),
+    tabBarIcon: ({ color, size }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -24,7 +80,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={createScreenOptions}>
+            <Tab.Screen name="Restaurants" component={RestaurantScreen} />
+            <Tab.Screen name="Maps" component={MapsScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
