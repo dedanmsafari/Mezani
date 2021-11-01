09f9ff28bc02417d05720e0273808daf498d1377
dedanmsafari <dedan.msafari@gmail.com>
2021-11-01 08:51:32 +0300

    restaurant screen modification

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import styled from "styled-components/native";
 import Search from "../../../components/Search/search";
@@ -6,41 +6,50 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 import { FlatList } from "react-native";
 import { Spacer } from "../../../components/Spacer/spacer.component";
 import { SafeAreaView } from "../../../utils/safeArea.util.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { LoadingIndicator } from "../../../components/ActivityIndicator/loadingIndicator.component";
 
 const SearchView = styled.View`
-  padding: ${(props) => props.theme.space[3]};
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-left: ${(props) => props.theme.space[3]};
+  padding-right: ${(props) => props.theme.space[3]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 },
 })``;
 
+const LoadingContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const RestaurantScreen = () => {
+  const { restaurants, loading, error } = useContext(RestaurantsContext);
   return (
-    <SafeAreaView>
+    <>
       <SearchView>
         <Search placeHolder="Search" />
       </SearchView>
-      <RestaurantList
-        data={[
-          { name: 1 },
-          { name: 2 },
-          { name: 3 },
-          { name: 4 },
-          { name: 5 },
-          { name: 6 },
-          { name: 7 },
-          { name: 8 },
-          { name: 9 },
-          { name: 10 },
-        ]}
-        renderItem={() => (
-          <Spacer position="bottom" size="large">
-            <RestaurantInfoCard />
-          </Spacer>
-        )}
-        keyExtractor={(item) => item.name}
-      />
-    </SafeAreaView>
+      {loading ? (
+        <LoadingContainer>
+          <LoadingIndicator />
+        </LoadingContainer>
+      ) : (
+        <RestaurantList
+          data={restaurants}
+          renderItem={({ item }) => {
+            return (
+              <Spacer position="bottom" size="large">
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            );
+          }}
+          keyExtractor={(item) => item.name}
+        />
+      )}
+    </>
   );
 };
