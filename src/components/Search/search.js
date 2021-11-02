e65bb8e59df99ba5ceebf813a105d806509b3922
dedanmsafari <dedan.msafari@gmail.com>
2021-11-02 21:42:16 +0300

    hooked Serach to location Context

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,15 +1,31 @@
 import * as React from "react";
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+import { LocationContext } from "../../services/location/location.context";
+
+const SearchView = styled.View`
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-left: ${(props) => props.theme.space[3]};
+  padding-right: ${(props) => props.theme.space[3]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
 
 const Search = ({ placeHolder }) => {
-  const [search, setSearch] = React.useState("");
+  const { search, keyword } = React.useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = React.useState(keyword);
+
 
   return (
-    <Searchbar
-      placeholder={placeHolder}
-      onChangeText={(text) => setSearch(text)}
-      value={search}
-    />
+    <SearchView>
+      <Searchbar
+        placeholder={placeHolder}
+        onSubmitEditing={() => search(searchKeyword)}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+        value={searchKeyword}
+      />
+    </SearchView>
   );
 };
 
