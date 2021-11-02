a0750cc73bb159fea41dda14d523cb9b0f18392c
dedanmsafari <dedan.msafari@gmail.com>
2021-11-02 21:43:06 +0300

    modified location Services

---
+++ src/services/location/location.service.js
@@ -0,0 +1,20 @@
+import camelize from "camelize";
+import { locations } from "./location.mock";
+
+export const locationRequest = (searchTerm) => {
+  return new Promise((resolve, reject) => {
+    const locationData = locations[searchTerm];
+    if (!locationData) {
+      reject("Not Found");
+    }
+    resolve(locationData);
+  });
+};
+
+export const locationTransform = (result) => {
+  const formattedResponse = camelize(result);
+  const { geometry = {} } = formattedResponse.results[0];
+  const { lng, lat } = geometry.location;
+
+  return { lng, lat };
+};
