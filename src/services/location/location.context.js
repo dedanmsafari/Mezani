c8614d5dc2e7e85af1796a3485332728084dae7f
dedanmsafari <dedan.msafari@gmail.com>
2021-11-02 21:42:49 +0300

    Created the location Context

---
+++ src/services/location/location.context.js
@@ -0,0 +1,44 @@
+import React, { useState, createContext } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [keyword, setKeyword] = useState("San francisco");
+
+  const getLocation = async (searchKeyword) => {
+    try {
+      const data = await locationRequest(searchKeyword.toLowerCase());
+      const locationData = locationTransform(data);
+      setLoading(false);
+      setLocation(locationData);
+      console.log(locationData);
+    } catch (err) {
+      setLoading(false);
+      setError(err);
+    }
+  };
+
+  React.useEffect(() => {
+    if (!keyword.length) {
+      return;
+    }
+    getLocation(keyword);
+  }, [keyword]);
+
+  const onSearch = (searchKeyword) => {
+    setLoading(true);
+    setKeyword(searchKeyword);
+  };
+  return (
+    <LocationContext.Provider
+      value={{ location, search: onSearch, loading, error, keyword }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
