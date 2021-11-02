961efe71c392a1ce94489e6fea8a570ad44512b5
dedanmsafari <dedan.msafari@gmail.com>
2021-11-02 21:43:52 +0300

    restaurant Screen

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -5,16 +5,8 @@ import Search from "../../../components/Search/search";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { FlatList } from "react-native";
 import { Spacer } from "../../../components/Spacer/spacer.component";
-import { SafeAreaView } from "../../../utils/safeArea.util.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { LoadingIndicator } from "../../../components/ActivityIndicator/loadingIndicator.component";
-
-const SearchView = styled.View`
-  padding-top: ${(props) => props.theme.space[1]};
-  padding-left: ${(props) => props.theme.space[3]};
-  padding-right: ${(props) => props.theme.space[3]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 },
@@ -27,12 +19,10 @@ const LoadingContainer = styled.View`
 `;
 
 export const RestaurantScreen = () => {
-  const { restaurants, loading, error } = useContext(RestaurantsContext);
+  const { restaurants, loading } = useContext(RestaurantsContext);
   return (
     <>
-      <SearchView>
-        <Search placeHolder="Search" />
-      </SearchView>
+      <Search placeHolder="Search location" />
       {loading ? (
         <LoadingContainer>
           <LoadingIndicator />
