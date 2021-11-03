bb90a62c98c01995dc89ce35b5b5e7fdbca1c04c
dedanmsafari <dedan.msafari@gmail.com>
2021-11-03 18:39:07 +0300

    added menuItem component

---
+++ src/components/MenuItems/menuItems.component.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { List } from "react-native-paper";
+import { ScrollView } from "react-native";
+
+export const MenuItems = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handlePress = () => setExpanded(!expanded);
+
+  return (
+    <ScrollView>
+      <List.Section title=" Menu ">
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="bread-slice" />}
+        >
+          <List.Item title="Toast Sandwitch" />
+          <List.Item title="Milk and Cereals" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="food" />}
+        >
+          <List.Item title="Rice and Beans" />
+          <List.Item title="Orange Blend Juice" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Dinner"
+          left={(props) => <List.Icon {...props} icon="food-variant" />}
+        >
+          <List.Item title="Brocolli and Spinatch" />
+          <List.Item title="Chicken soup" />
+        </List.Accordion>
+        <List.Accordion
+          title="Drinks"
+          left={(props) => <List.Icon {...props} icon="glass-cocktail" />}
+          expanded={expanded}
+          onPress={handlePress}
+        >
+          <List.Item title="Fresh Mango Juice" />
+          <List.Item title="Red Wine" />
+        </List.Accordion>
+      </List.Section>
+    </ScrollView>
+  );
+};
