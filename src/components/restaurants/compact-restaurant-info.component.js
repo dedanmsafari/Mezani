0a4314ccad7023a4c4cea100353c1605483fb946
dedanmsafari <dedan.msafari@gmail.com>
2021-11-04 23:03:53 +0300

    created compact restaurant info

---
+++ src/components/restaurants/compact-restaurant-info.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { Text } from "../../components/Text/text.component";
+import { WebView } from "react-native-webview";
+import { Platform } from "react-native";
+
+const isAndrioid = Platform.OS === "android";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebView = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndrioid ? CompactWebView : CompactImage;
+  return (
+    <Item>
+      <Image source={{ uri: restaurant.photos[0] }} />
+      <Text center variant="caption" numberOfLines={3}>
+        {restaurant.name}
+      </Text>
+    </Item>
+  );
+};
