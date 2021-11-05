a5c372440957ad31beebe64654ed0ef528c26602
dedanmsafari <dedan.msafari@gmail.com>
2021-11-05 10:23:02 +0300

    restaurants navigator component changes

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -4,27 +4,29 @@ import {
   TransitionPresets,
 } from "@react-navigation/stack";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
-import { Text } from "react-native";
+import { SafeAreaView } from "../../utils/safeArea.util.component";
 import { RestaurantsDetailsScreen } from "../../features/restaurants/screens/restaurantsDetails.screen";
 
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator
-      screenOptions={{
-        headerShown: false,
-        ...TransitionPresets.ModalPresentationIOS,
-      }}
-    >
-      <RestaurantStack.Screen
-        name="restaurants"
-        component={RestaurantsScreen}
-      />
-      <RestaurantStack.Screen
-        name="restaurantDetails"
-        component={RestaurantsDetailsScreen}
-      />
-    </RestaurantStack.Navigator>
+    <SafeAreaView>
+      <RestaurantStack.Navigator
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.ModalPresentationIOS,
+        }}
+      >
+        <RestaurantStack.Screen
+          name="restaurants"
+          component={RestaurantsScreen}
+        />
+        <RestaurantStack.Screen
+          name="restaurantDetails"
+          component={RestaurantsDetailsScreen}
+        />
+      </RestaurantStack.Navigator>
+    </SafeAreaView>
   );
 };
