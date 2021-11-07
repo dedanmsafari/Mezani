540380a4624c05107ab2c6494e8c03d13c945c32
dedanmsafari <dedan.msafari@gmail.com>
2021-11-07 19:29:22 +0300

    app navigator changes

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -5,16 +5,9 @@ import { RestaurantsNavigator } from "./restaurants.navigator";
 import { SafeAreaView } from "../../utils/safeArea.util.component";
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "react-native";
+import { MapScreen } from "../../features/map/screens/map.screens";
 
 const Tab = createBottomTabNavigator();
-
-function MapsScreen() {
-  return (
-    <SafeAreaView>
-      <Text>Maps</Text>
-    </SafeAreaView>
-  );
-}
 
 function SettingsScreen() {
   return (
@@ -44,14 +37,12 @@ const createScreenOptions = ({ route }) => {
 
 export const AppNavigator = () => {
   return (
-    <SafeAreaView>
-      <NavigationContainer>
-        <Tab.Navigator screenOptions={createScreenOptions}>
-          <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-          <Tab.Screen name="Maps" component={MapsScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={createScreenOptions}>
+        <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+        <Tab.Screen name="Maps" component={MapScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
