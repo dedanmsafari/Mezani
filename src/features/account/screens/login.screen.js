d96315a03594fe11b5f6d56827ec12266a07c172
dedanmsafari <dedan.msafari@gmail.com>
2021-11-08 18:57:26 +0300

    login Screen changes

---
+++ src/features/account/screens/login.screen.js
@@ -0,0 +1,70 @@
+import React, { useState, useContext } from "react";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  FormInput,
+  Title,
+  ErrorContainer,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/Spacer/spacer.component";
+import { Text } from "../../../components/Text/text.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+export const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { onLogin, error } = useContext(AuthenticationContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>Mezani</Title>
+      <AccountContainer>
+        <FormInput
+          label="Email"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(t) => setEmail(t)}
+        />
+
+        <Spacer size="large">
+          <FormInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        {error && (
+          <Spacer size="large">
+            <ErrorContainer>
+              <Text variant="error">{error}</Text>
+            </ErrorContainer>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
+    </AccountBackground>
+  );
+};
