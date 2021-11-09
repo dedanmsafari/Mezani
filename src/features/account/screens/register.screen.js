2e2da608a5aa68d6400325b82394171e319c60f7
dedanmsafari <dedan.msafari@gmail.com>
2021-11-09 07:22:39 +0300

    register screen component

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,6 +1,86 @@
-import { AccountBackground } from "../components/account.styles";
-import React from "react";
+import React, { useState, useContext } from "react";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  FormInput,
+  Title,
+  ErrorContainer,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/Spacer/spacer.component";
+import { Text } from "../../../components/Text/text.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { LoadingIndicator } from "../../../components/ActivityIndicator/loadingIndicator.component";
 
-export const RegisterScreen = () => {
-  return <AccountBackground />;
+export const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+
+  const { onRegister, isLoading, error } = useContext(AuthenticationContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>Mezani</Title>
+      <AccountContainer>
+        <FormInput
+          label="Email"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(t) => setEmail(t)}
+        />
+
+        <Spacer size="large">
+          <FormInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        <Spacer size="large">
+          <FormInput
+            label="Repeat Password"
+            value={repeatedPassword}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(p) => setRepeatedPassword(p)}
+          />
+        </Spacer>
+
+        {error && (
+          <Spacer size="large">
+            <ErrorContainer>
+              <Text variant="error">{error}</Text>
+            </ErrorContainer>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          {!isLoading ? (
+            <AuthButton
+              icon="email"
+              mode="contained"
+              onPress={() => onRegister(email, password, repeatedPassword)}
+            >
+              Register
+            </AuthButton>
+          ) : (
+            <LoadingIndicator />
+          )}
+        </Spacer>
+      </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
+    </AccountBackground>
+  );
 };
