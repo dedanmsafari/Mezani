af8f62bf014a4eead6e068e788f412ac94a695f9
dedanmsafari <dedan.msafari@gmail.com>
2021-11-09 07:20:43 +0300

    modified favourited Context

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,10 +1,12 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useEffect, useContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthenticationContext } from "../authentication/authentication.context";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = React.useState([]);
+  const { user } = useContext(AuthenticationContext);
 
   const addFavourite = (favourite) => {
     setFavourites([...favourites, favourite]);
@@ -14,18 +16,18 @@ export const FavouritesContextProvider = ({ children }) => {
     setFavourites(favourites.filter((f) => f.placeId !== favourite.placeId));
   };
 
-  const storeFavourites = async () => {
+  const storeFavourites = async (value, uid) => {
     try {
-      const favouritesStorage = JSON.stringify(favourites);
-      await AsyncStorage.setItem("favourites", favouritesStorage);
+      const favouritesStorage = JSON.stringify(value);
+      await AsyncStorage.setItem(`@favourites-${uid}`, favouritesStorage);
     } catch (e) {
       console.log("An Error Occured while saving Favourites", e);
     }
   };
 
-  const retrieveFavourites = async () => {
+  const retrieveFavourites = async (uid) => {
     try {
-      const favouriteItems = await AsyncStorage.getItem("favourites");
+      const favouriteItems = await AsyncStorage.getItem(`@favourites-${uid}`);
       if (favouriteItems !== null) {
         const foundfavourites = JSON.parse(favouriteItems);
         return setFavourites(foundfavourites);
@@ -36,12 +38,16 @@ export const FavouritesContextProvider = ({ children }) => {
     }
   };
   useEffect(() => {
-    retrieveFavourites();
-  }, []);
+    if (user && user.uid) {
+      retrieveFavourites(user.uid);
+    }
+  }, [user]);
 
   useEffect(() => {
-    storeFavourites(favourites);
-  }, [favourites]);
+    if (user && user.uid && favourites.length) {
+      storeFavourites(favourites, user.uid);
+    }
+  }, [favourites, user]);
   return (
     <FavouritesContext.Provider
       value={{
