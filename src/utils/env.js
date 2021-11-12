c2832036881375b9e3063711ee00af97be1b85d1
dedanmsafari <dedan.msafari@gmail.com>
2021-11-12 04:54:49 +0300

    envionments variable

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,13 @@
+import { Platform } from "react-native";
+
 //http urls dont work on android must use liveHost to work
 
 const liveHost = "https://us-central1-mezani-64ebd.cloudfunctions.net";
 const localHost = "http://localhost:5001/mezani-64ebd/us-central1";
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const host = liveHost;
-// if on IOS USE export const host = isDevelopment ? localHost : liveHost;
+// export const host = liveHost;
+export const isAndroid = Platform.OS === "android";
+
+export const host = !isDevelopment || isAndroid ? liveHost : localHost;
+
+export const isMock = false;
