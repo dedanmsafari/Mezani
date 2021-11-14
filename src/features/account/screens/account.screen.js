84c7ce55b5bed6a50dcb0c408b0e3701c608df24
dedanmsafari <dedan.msafari@gmail.com>
2021-11-14 19:47:50 +0300

    account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -5,13 +5,25 @@ import {
   AccountCover,
   AuthButton,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
+
 import { Spacer } from "../../../components/Spacer/spacer.component";
+import LottieView from "lottie-react-native";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          source={require("../../../../assets/watermelon.json")}
+          autoPlay
+          loop
+          resizeMode="cover"
+        />
+      </AnimationWrapper>
       <Title> Mezani</Title>
       <AccountContainer>
         <AuthButton
