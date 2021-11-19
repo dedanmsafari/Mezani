79a2a8b2b7c49f31ebce5967dbbc5ce76070467a
dedanmsafari <dedan.msafari@gmail.com>
2021-11-19 07:26:11 +0300

    favourites bar edit styles

--- src/components/Favourites/favouritesBar.component.js
+++ src/components/Favourites/favouritesBar.component.js
@@ -2,10 +2,13 @@ import React from "react";
 import styled from "styled-components/native";
 import { ScrollView, TouchableOpacity } from "react-native";
 import { Spacer } from "../Spacer/spacer.component";
+import { Card } from "react-native-paper";
 import { CompactRestaurantInfo } from "../restaurants/compact-restaurant-info.component";
 import { Text } from "../Text/text.component";
-const FavouritesView = styled.View`
+const FavouritesView = styled(Card)`
   padding: 10px;
+  z-index: 999;
+  border-radius: 15px;
 `;
 
 export const FavouritesBar = ({ favourites, navigation }) => {
@@ -22,7 +25,7 @@ export const FavouritesBar = ({ favourites, navigation }) => {
   }
 
   return (
-    <FavouritesView>
+    <FavouritesView elevation={3}>
       <Spacer position="left" size="large">
         <Text variant="caption">Favourites</Text>
       </Spacer>
