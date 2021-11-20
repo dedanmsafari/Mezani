65987e69dd0cec230a24729402617829fbe919b1
dedanmsafari <dedan.msafari@gmail.com>
2021-11-20 20:40:58 +0300

    minor index file change

---
+++ functions/pay/index.js
@@ -0,0 +1,22 @@
+module.exports.payRequest = (req, res, stripeClient) => {
+  const body = JSON.parse(req.body);
+  const { token, amount } = body;
+  stripeClient.paymentIntents
+    .create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+      payment_method_data: {
+        type: "card",
+        card: { token },
+      },
+      confirm: true,
+    })
+    .then((paymentIntent) => {
+      res.json(paymentIntent);
+    })
+    .catch((e) => {
+      res.status(400);
+      res.send(e);
+    });
+};
